eacdea42ea3d8356d20a211ffa39a2a2d21707f9
mariatj <tarsi.maria1@rigd-loxia.nl>
2023-05-31 16:14:59 +0200

    focus on textarea

--- src/app/components/add-modal/add-modal.component.ts
+++ src/app/components/add-modal/add-modal.component.ts
@@ -1,20 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { SupabaseService } from '../../services/supabase.service';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'tda-add-modal',
   templateUrl: './add-modal.component.html',
   styleUrls: ['./add-modal.component.scss']
 })
-export class AddModalComponent {
+export class AddModalComponent implements AfterViewInit {
+
+  @ViewChild('todotext')
+  textArea: ElementRef | null = null;
 
   @Output()
   close: EventEmitter<string> = new EventEmitter();
 
-  constructor(private supabaseService: SupabaseService) {
+  ngAfterViewInit(): void {
+    this.textArea?.nativeElement.focus();
   }
 
   handleClose(text?: string) {
     this.close.emit(text);
   }
+
 }
